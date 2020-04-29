c3402eab5acd96fb88ed3f6ef829a59050ea08a7
Will Humphlett <will@humphlett.net>
2020-04-29 02:52:32 -0400

    Port over second draft to initial name

--- Telegram-to-Terminal/tel_to_term.py
+++ Telegram-to-Terminal/tel_to_term.py
@@ -3,13 +3,14 @@ import sys
 import yaml
 import time
 import logging
-import subprocess
-from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
 
 class TelegramTerminal:
     def __init__(self):
         logging.basicConfig(filename=sys.path[0] + '/log.txt', level=logging.INFO)
+        logging.info('Starting Telegram to Terminal')
         with open(sys.path[0] + '/config.yml', 'r') as ymlfile:
             config = yaml.safe_load(ymlfile)
             self.token = config['token']
@@ -18,6 +19,7 @@ class TelegramTerminal:
             ymlfile.close()
         self.updater = Updater(token=self.token, use_context=True)
         self.init_dispatch()
+        self.superuser = ''
 
     def init_dispatch(self):
         dispatcher = self.updater.dispatcher
@@ -28,74 +30,96 @@ class TelegramTerminal:
         running_handler = CommandHandler('running', self.running)
         dispatcher.add_handler(running_handler)
 
-        log_handler = CommandHandler('log', self.log)
-        dispatcher.add_handler(log_handler)
-
         run_handler = CommandHandler('run', self.run)
         dispatcher.add_handler(run_handler)
 
         stop_handler = CommandHandler('stop', self.stop)
         dispatcher.add_handler(stop_handler)
 
-    @staticmethod
-    def start(update, context):
+        log_handler = CommandHandler('log', self.log)
+        dispatcher.add_handler(log_handler)
+
+        dispatcher.add_handler(CallbackQueryHandler(self.call_back))
+        dispatcher.add_error_handler(self.error)
+
+    def start(self, update, context):
         with open(sys.path[0] + '/config.yml', 'r') as ymlfile:
             superuser = yaml.safe_load(ymlfile)['username']
             ymlfile.close()
         if update.message.from_user.username != superuser:
             msg = 'You do not have permission to access this terminal'
+            context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
         else:
-            msg = 'Welcome Superuser'
-        context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
+            self.superuser = update.message.from_user.id
+            keyboard = [
+                [KeyboardButton('/running')],
+                [KeyboardButton('/run'), KeyboardButton('/stop')],
+                [KeyboardButton('/log'), KeyboardButton('/xxx')]
+            ]
+            kb_markup = ReplyKeyboardMarkup(keyboard)
+            context.bot.send_message(chat_id=update.message.chat_id, text='Welcome Superuser', reply_markup=kb_markup)
 
     def running(self, update, context):
-        output = os.popen('{}/bash_scripts/currently-running-tel'.format(self.base_path)).read()
-
+        if update.message.from_user.id == self.superuser:
+            output = os.popen('{}/bash_scripts/currently-running-tel'.format(self.base_path)).read()
+        else:
+            output = 'Permission Denied'
         context.bot.send_message(chat_id=update.effective_chat.id, text=output)
 
-    def log(self, update, context):
-        if len(context.args) < 1:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='Provide a script name fragment')
-            return
-        output = os.popen('grep -v grep {}/bash_scripts/1-config.txt | grep {}'.format(self.base_path, context.args[0]))
-        script = output.read()
-        script = script.replace('\n', '', 1)
-        if script == '' or script.count('\n') > 0:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='Invalid argument, does not specify script')
-        else:
-            if script.split(':')[2] == 'telegram':
-                output = 'Cannot view the log of the bot from the bot while the bot is running'
-            else:
-                output = os.popen('tail -n 20 {}/{}/log.txt'.format(self.base_path, script.split(':')[3])).read()
-            context.bot.send_message(chat_id=update.effective_chat.id, text=output)
-
     def run(self, update, context):
-        if len(context.args) < 1:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='Provide a script name fragment')
-            return
-        output = os.popen('grep -v grep {}/bash_scripts/1-config.txt | grep {}'.format(self.base_path, context.args[0]))
-        script = output.read()
-        script = script.replace('\n', '', 1)
-        if script == '' or script.count('\n') > 0:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='Invalid argument, does not specify script')
+        if update.message.from_user.id == self.superuser:
+            keyboard = self.get_options()
+            update.message.reply_text('/run <script>', reply_markup=keyboard)
         else:
-            os.system('{}/bash_scripts/{}'.format(self.base_path, script.split(':')[2]))
-            output = 'Running script {}'.format(script.split(':')[2])
-            context.bot.send_message(chat_id=update.effective_chat.id, text=output)
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Permission Denied')
 
     def stop(self, update, context):
-        if len(context.args) < 1:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='Provide a script name fragment')
-            return
-        output = os.popen('grep -v grep {}/bash_scripts/1-config.txt | grep {}'.format(self.base_path, context.args[0]))
-        script = output.read()
-        script = script.replace('\n', '', 1)
-        if script == '' or script.count('\n') > 0:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='Invalid argument, does not specify script')
+        if update.message.from_user.id == self.superuser:
+            keyboard = self.get_options()
+            update.message.reply_text('/stop <script>', reply_markup=keyboard)
         else:
-            os.system('{}/bash_scripts/{}-kill'.format(self.base_path, script.split(':')[2]))
-            output = 'Running script {}-kill'.format(script.split(':')[2])
-            context.bot.send_message(chat_id=update.effective_chat.id, text=output)
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Permission Denied')
+
+    def log(self, update, context):
+        if update.message.from_user.id == self.superuser:
+            keyboard = self.get_options()
+            update.message.reply_text('/log <script>', reply_markup=keyboard)
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Permission Denied')
+
+    def call_back(self, update, context):
+        query = update.callback_query
+        query.answer()
+        command = '{} {}'.format(query.message.text.split()[0], query.data)
+        query.edit_message_text(text=command)
+        time.sleep(1)
+        dir_name = os.popen('grep -v grep {}/bash_scripts/1-config.txt | grep {}'.format(self.base_path, query.data))\
+            .read().replace('\n', '', 1).split(':')[3]
+        cmd_dict = {
+            '/run': '{}/bash_scripts/{}'.format(self.base_path, query.data),
+            '/stop': '{}/bash_scripts/{}-kill'.format(self.base_path, query.data),
+            '/log': 'tail -n 20 {}/{}/log.txt'.format(self.base_path, dir_name)
+        }
+        cmd_return = os.popen(cmd_dict[query.message.text.split()[0]]).read()
+        query.edit_message_text(text=cmd_return)
+
+    def get_options(self):
+        output = os.popen('grep -v grep {}/bash_scripts/1-config.txt | grep -v ^#'.format(self.base_path)).read()
+        options = []
+        for line in output.split('\n')[:-1]:
+            options.append(InlineKeyboardButton(line.split(':')[2], callback_data=line.split(':')[2]))
+        keyboard = []
+        if len(options) % 2 == 0:
+            for i in range(0, len(options), 2):
+                keyboard.append([options[i], options[i+1]])
+        else:
+            for i in range(0, len(options)-1, 2):
+                keyboard.append([options[i], options[i + 1]])
+            keyboard.append([options[-1]])
+        return InlineKeyboardMarkup(keyboard)
+
+    def error(self, update, context):
+        logging.warning('Error: {}'.format(update, context.error))
 
 
 if __name__ == '__main__':
